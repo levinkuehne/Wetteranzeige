0193515ad0103dbee2b9cc1904c947073b22e227
levinkuehne <kuehnelevin@gmail.com>
2021-10-12 10:12:56 +0200

    Textfeld eingefügt

--- sketch_211012a/sketch_211012a.js
+++ sketch_211012a/sketch_211012a.js
@@ -6,9 +6,15 @@ function setup() {
   background("lightblue");
   frameRate(120);
   
+  let APIKEY = "6c897f070ff5c68387abf0306fedce0c";
+  let city = createInput("Bitte geben Sie Ihre Stadt ein.");
+  city.position(400, 50);
+  city.size(100);
+  city.input(Textfeld);
+  
 }
 
 
-function draw() {
-  
+function Textfeld() {
+  console.log('you are typing: ', this.value());
 }
