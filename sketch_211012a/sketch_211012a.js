74ea26b42778722e6bdd4f6d309a3a015a5e0192
levinkuehne <kuehnelevin@gmail.com>
2021-10-12 14:10:40 +0200

    Texteingabefeld funktionsfähig

--- sketch_211012a/sketch_211012a.js
+++ sketch_211012a/sketch_211012a.js
@@ -1,3 +1,6 @@
+let APIKEY = "6c897f070ff5c68387abf0306fedce0c";
+let city;
+
 function setup() {
   
  //Hintergrund
@@ -8,28 +11,41 @@ function setup() {
     
  //Grass
   strokeWeight(0);
-  {fill(154,205,50);
+  
+  fill(154,205,50);
   rect(0,450,1000,50);
   ellipse(100, 450, 390, 120);
   ellipse(400, 450, 475, 90);
   ellipse(750, 450 , 550, 100);
-  }
   
-  let url = httpGet();
-  let APIKEY = "6c897f070ff5c68387abf0306fedce0c";
-  let city = createInput("");
+  
+  city = createInput("");
   city.position(25, 25);
   city.size(175);
-  city.input(Textfeld);
-  url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKEY;
+  city.input(myInputEvent);
   
+  button = createButton('submit');
+  button.position(200, 25, 65);
+  button.mousePressed(mySubmitEvent);
+  
+}
+
+function mySubmitEvent(){
+  const name = city.value();
+   queryAPI(name);
+}
+
+
+function myInputEvent() {
+  console.log('you are typing: ', this.value());
+}
+
+function queryAPI(value) {
+  let url = "https://api.openweathermap.org/data/2.5/weather?q="+value+"&appid="+APIKEY;
   httpGet(url, 'jsonp', false, function(response) {
     console.log(response);
   });
+}
 
-function Textfeld() {
-  console.log('you are typing: ', this.value());
-}
-}
 function draw() {
 }
