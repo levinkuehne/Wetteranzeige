c817b436abdf0236031231f792d0f42571f004b5
Jerome Schiesser <schiesser@netcloud.ch>
2021-10-12 11:11:30 +0200

    API URL eingefügt

--- sketch_211012a/sketch_211012a.js
+++ sketch_211012a/sketch_211012a.js
@@ -14,14 +14,18 @@ function setup() {
   ellipse(400, 450, 475, 90);
   ellipse(750, 450 , 550, 100);
   
+  let url = httpGet();
   let APIKEY = "6c897f070ff5c68387abf0306fedce0c";
   let city = createInput("Bitte geben Sie eine Stadt ein.");
   city.position(750, 50);
   city.size(175);
   city.input(Textfeld);
+  url = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+APIKEY;
   
+  httpGet(url, 'jsonp', false, function(response) {
+    console.log(response);
+  });
 }
-
 
 function Textfeld() {
   console.log('you are typing: ', this.value());
