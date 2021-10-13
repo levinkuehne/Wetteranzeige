8efc9b2e72b3d983860df03b50cd1cb65845cfc9
RobinGraf <robin.graf05@gmail.com>
2021-10-13 11:41:40 +0200

    Gewitter

---
+++ sketch_211013aBlitz/sketch_211013a_Blitz/sketch_211013a_Blitz.js
@@ -0,0 +1,38 @@
+function setup() {
+
+  //Hntergrund
+  
+  createCanvas(1000, 500);
+  background("lightblue");
+  frameRate(120);
+  
+  //Blitz
+strokeWeight(25);
+stroke("yellow");
+line(880, 140, 860, 190);
+line(860, 190, 910, 180);
+line(910, 180, 890, 230);
+
+  
+  //Wolke
+ 
+  noStroke();
+  rect(840, 118, 80, 40);
+  circle(920, 105, 50);
+  circle(890, 100, 50);
+  circle(860, 110, 50);
+  circle(835, 125, 50);
+  circle(935, 135, 50);
+  circle(920, 135, 50);
+  circle(845, 135, 50);
+  circle(870, 140, 50);
+  circle(890, 138, 50);
+  
+
+
+}
+
+
+function draw() {
+
+}
