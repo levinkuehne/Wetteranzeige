6cb3e18f962cc1d8ddc1c76e716edad4d5815b7e
Jerome Schiesser <schiesser@netcloud.ch>
2021-10-13 13:04:22 +0200

    Ordner richitg benannt

---
+++ sketch_211013b/sketch_211013b_pde/sketch_211013b_pde.js
@@ -0,0 +1,24 @@
+function setup() {
+  createCanvas(1000, 500);
+  background(135, 206, 250);
+}
+function draw() {
+  stroke(195);
+  strokeWeight(15);
+  line(650,200,900,200);
+  
+
+  strokeWeight(15);
+  line(600,160,800,160);
+  
+ 
+  strokeWeight(15);
+  line(700,120,910,120);
+  
+ 
+  strokeWeight(15);
+  line(620,240,840,240);
+  
+  strokeWeight(15);
+  line(700,280,860,280);
+}
