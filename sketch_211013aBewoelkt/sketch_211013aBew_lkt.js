6743c8f418ad7014ea8fe35497c5e7129ce85c5f
RobinGraf <robin.graf05@gmail.com>
2021-10-13 12:55:59 +0200

    Bewoelkt

---
+++ sketch_211013aBewoelkt/sketch_211013aBew_lkt.js
@@ -0,0 +1,48 @@
+function setup() {
+
+  //Hntergrund
+  
+  createCanvas(1000, 500);
+  background("lightblue");
+  frameRate(120);
+  
+  
+    //Wolke hinten
+    
+  fill("lightgray");
+  noStroke();
+  rect(860, 108, 80, 40);
+  
+  circle(940, 95, 50);
+  circle(910, 90, 50);
+  circle(880, 100, 50);
+  circle(855, 115, 50);
+  circle(955, 125, 50);
+  circle(940, 125, 50);
+  circle(865, 125, 50);
+  circle(890, 130, 50);
+  circle(910, 128, 50);
+
+    //Wolke vorne
+ 
+  fill("wight");
+  noStroke();
+  rect(840, 118, 80, 40);
+  
+  circle(920, 105, 50);
+  circle(890, 100, 50);
+  circle(860, 110, 50);
+  circle(835, 125, 50);
+  circle(935, 135, 50);
+  circle(920, 135, 50);
+  circle(845, 135, 50);
+  circle(870, 140, 50);
+  circle(890, 138, 50);
+ 
+  
+}
+
+
+function draw() {
+
+}
