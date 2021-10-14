125eaac303a8c17f394444a64b0a0867bfdc7113
RobinGraf <robin.graf05@gmail.com>
2021-10-14 14:22:15 +0200

    lange Hose

---
+++ Wetterlagen_und_Kleidungsstuecke/lange_Hose/lange_Hose.js
@@ -0,0 +1,33 @@
+function setup() {
+
+   //Hntergrund
+  
+  createCanvas(1000, 500);
+  background("lightblue");
+ 
+ //Kleidung 
+ strokeWeight(10);
+ stroke("black");
+ 
+ //Hose
+ line(790, 260, 880, 260);
+ line(790, 260, 760, 450);
+ line(760, 450, 815, 450);
+ line(815, 450, 835, 320);
+ line(835, 320, 855, 450);
+ line(855, 450, 910, 450);
+ line(910, 450, 880, 260);
+ 
+ //Taschen
+ line(790, 300, 810, 300);
+ line(810, 300, 815, 260);
+
+line(880, 300, 860, 300);
+line(860, 300, 855, 260);
+
+}
+
+
+function draw() {
+
+}
