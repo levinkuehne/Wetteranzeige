92d29f45ba41f6ce2401f27b8553bdde7449f6fc
RobinGraf <robin.graf05@gmail.com>
2021-10-14 13:40:13 +0200

    kurze Hose und alle Kleidungstücke verschoben

---
+++ Wetterlagen_und_Kleidungsstuecke/kurze_Hose/kurze_Hose.js
@@ -0,0 +1,35 @@
+function setup() {
+ 
+  //Hntergrund
+  
+  createCanvas(1000, 500);
+  background("lightblue");
+ 
+ //Kleidung 
+ strokeWeight(10);
+ stroke("black");
+ 
+ //Hose
+ line(750, 340, 840, 340);
+ line(750, 340, 720, 450);
+ line(720, 450, 775, 450);
+ line(775, 450, 795, 400);
+ line(795, 400, 815, 450);
+ line(815, 450, 870, 450);
+ line(870, 450, 840, 340);
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+}
+
+
+function draw() {
+
+}
